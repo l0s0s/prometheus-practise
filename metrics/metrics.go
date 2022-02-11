effe91dc65e542c77ce47a3fb9a61ea7743c9a01
Pavel Ohursov <cucumb3rpasha@gmail.com>
2022-02-11 12:05:42 +0200

    prometheus -> promauto

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -2,19 +2,20 @@ package metrics
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 var (
 	// Status200 is the response metric with code 200.
-	Status200 = prometheus.NewCounter(prometheus.CounterOpts{
+	Status200 = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "http_200_status_code_response",
 	})
 	// Status401 is the response metric with code 401.
-	Status401 = prometheus.NewCounter(prometheus.CounterOpts{
+	Status401 = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "http_401_status_code_response",
 	})
 	// Status404  is the response metric with code 404.
-	Status404 = prometheus.NewCounter(prometheus.CounterOpts{
+	Status404 = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "http_404_status_code_response",
 	})
 )
